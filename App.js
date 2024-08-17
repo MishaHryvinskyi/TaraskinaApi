17e6318380811cd7a5761d7a23d2810154b78b1d
Misha <hryvinskyim@gmail.com>
2024-08-17 16:34:20 +0300

    start create api

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const books =require("./books/book");
+const cors = require("cors");
+
+const booksRouter = require("./routes/api/books");
 
 const app = express(); // app - веб-сервер
 
-app.use((req, res, next) => {
-    next();
-});
+app.use(cors());
 
-app.get("/books", (req, res) => {
-    res.json(books);
-});
+app.use("/api/books", booksRouter);
 
 app.use((req, res) => {
-    res.status(404).json({ 
-        message: "Not Found" 
-    });
+    res.status(404).json({ message: 'Not found' });
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message })
 })
 
 app.listen(3000);
--- books/index.js
+++ books/index.js
@@ -1,18 +1,56 @@
 const fs = require('fs/promises');
 const path = require('path');
+const { nanoid } = require("nanoid");
 
-const workPath = path.join(__dirname, "book.json")
+const booksPath = path.join(__dirname, "book.json")
 
 const getAll = async () => {
-   const data = await fs.readFile(workPath, 'utf-8');
+   const data = await fs.readFile(booksPath);
    return JSON.parse(data);
 };
 
-const getById = async ({ id }) => {
-    const data = await fs.readFile(`${__dirname}/book.json`, 'utf-8');
-    return JSON.parse(data);
- };
+const getById = async (id) => {
+   const books = await getAll();
+   const result = books.find(item => item.id === id);
+   return result || null;
+};
 
+const add = async(data) => {
+    const books = await getAll();
+    const newBook = {
+        id: nanoid(),
+        ...data,
+    }
+    books.push(newBook);
+    await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+    return newBook;
+};
+
+const updateById = async(id, data) => {
+    const books = await getAll();
+    const index = books.findIndex(item => item.id === id);
+    if(index === -1) {
+        return null;
+    }
+    books[index] = { id, ...data };
+    await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+    return books[index];
+};
+
+const deleteById = async(id, data) => {
+    const books = await getAll();
+    const index = books.findIndex(item => item.id === id);
+    if(index === -1) {
+        return null;
+    }
+    const [result] = books.splice(index, 1);
+    await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+    return result;
+};
 module.exports = {
     getAll,
-}
+    getById,
+    add,
+    updateById,
+    deleteById,
+};
